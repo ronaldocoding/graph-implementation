3641950d8901bdeca331e30fbc74e5f426e0dfa6
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-01-22 14:48:24 -0400

    Check if a graph is hamiltonian by ore theorem

--- graph.c
+++ graph.c
@@ -5,6 +5,8 @@
 typedef struct vertex
 {
     int id;
+    bool visited;
+    int out_degree;
     node *adj_list;
 } vertex;
 
@@ -209,46 +211,46 @@ int main()
     insert_vertex(&list1);
 
     /*
-     * Adiciona uma aresta bidirecional entre os vértices de índices 0 e 1 
+     * Adiciona uma aresta bidirecional entre os vértices de índices 0 e 1
      * de peso 10
-    */ 
+     */
     insert_edge(&list1, 0, 1, true, 10);
 
     /*
      * Adiciona uma aresta unidirecional entre os vértices de índices 0 e 2, ou
      * seja, é possível ir do vértice de índice 0 para o de índice 2, mas
      * não o contrário (até o momento). Essa aresta tem peso 0
-    */ 
+     */
     insert_edge(&list1, 0, 2, false, 0);
 
     /*
-     * Adiciona uma aresta bidirecional entre os vértices de índices 0 e 3 
+     * Adiciona uma aresta bidirecional entre os vértices de índices 0 e 3
      * de peso 20
-    */ 
+     */
     insert_edge(&list1, 0, 3, true, 20);
 
     /*
-     * Adiciona uma aresta bidirecional entre os vértices de índices 1 e 3 
+     * Adiciona uma aresta bidirecional entre os vértices de índices 1 e 3
      * de peso 5
-    */ 
+     */
     insert_edge(&list1, 1, 3, true, 5);
 
     /*
-     * Adiciona uma aresta bidirecional entre os vértices de índices 1 e 4 
+     * Adiciona uma aresta bidirecional entre os vértices de índices 1 e 4
      * de peso 12
-    */ 
+     */
     insert_edge(&list1, 1, 4, true, 12);
 
     /*
-     * Adiciona uma aresta unidirecional entre os vértices de índices 2 e 3 
+     * Adiciona uma aresta unidirecional entre os vértices de índices 2 e 3
      * de peso 8
-    */ 
+     */
     insert_edge(&list1, 2, 3, false, 8);
 
     /*
-     * Adiciona uma aresta unidirecional entre os vértices de índices 3 e 4 
+     * Adiciona uma aresta unidirecional entre os vértices de índices 3 e 4
      * de peso 18
-    */ 
+     */
     insert_edge(&list1, 3, 4, false, 18);
 
     // Mostra o grafo
@@ -279,17 +281,17 @@ int main()
     printf("=================\n");
 
     /*
-     * Adiciona uma aresta bidirecional entre os vértices de índices 2 e 7 
+     * Adiciona uma aresta bidirecional entre os vértices de índices 2 e 7
      * de peso 30
-    */ 
+     */
     insert_edge(&list1, 2, 7, true, 30);
 
     /*
-     * Tenta adicionar uma aresta bidirecional entre os vértices de índices 2 e 4 
+     * Tenta adicionar uma aresta bidirecional entre os vértices de índices 2 e 4
      * de peso 16, mas como o vértice de índice 4 não existe, essa aresta não é
      * adicionada
-    */
-    insert_edge(&list1, 2, 4, true  , 16);
+     */
+    insert_edge(&list1, 2, 4, true, 16);
 
     // Mostra o grafo
     show_graph(list1);
@@ -308,5 +310,5 @@ int main()
      * não existe um sentido entre as arestas. No entanto, quando inserimos no
      * mínimo uma aresta unidirecional, temos um grafo direcionado, mesmo que
      * todas as outras arestas sejam bidirecionais
-    */
+     */
 }
