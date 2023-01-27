6455d603267da9f7354b3a52f9bc15f8fcb41a21
tmmarquess <thiago.uchoa18@gmail.com>
2023-01-27 03:02:10 -0400

    algoritmo de prim funcionando

--- prim.c
+++ prim.c
@@ -1,24 +1,29 @@
 #include "graph.c"
+#include <math.h>
 
 typedef struct priority_queue
 {
-    node edge;
+    vertex *current_vertex;
+    int key;
+    vertex *parent;
     struct priority_queue *next;
 } priority_queue;
 
-priority_queue *new_item(node edge)
+priority_queue *new_item(vertex *vertex)
 {
     priority_queue *new_item = malloc(sizeof(priority_queue));
-    new_item->edge = edge;
+    new_item->current_vertex = vertex;
+    new_item->key = (int)INFINITY;
+    new_item->parent = NULL;
     new_item->next = NULL;
     return new_item;
 }
 
-void enqueue(priority_queue **queue, node edge)
+void enqueue(priority_queue **queue, vertex *vertex)
 {
     if ((*queue) == NULL)
     {
-        (*queue) = new_item(edge);
+        (*queue) = new_item(vertex);
         return;
     }
 
@@ -27,18 +32,61 @@ void enqueue(priority_queue **queue, node edge)
     {
         current = current->next;
     }
-    current->next = new_item(edge);
+    current->next = new_item(vertex);
 }
 
-node dequeue(priority_queue **queue)
+void enqueue_2(priority_queue **queue, priority_queue *item)
 {
-    node item = (*queue)->edge;
+    if ((*queue) == NULL)
+    {
+        (*queue) = item;
+        return;
+    }
 
-    priority_queue *new_next = (*queue)->next;
-    free((*queue));
-    (*queue) = new_next;
+    priority_queue *current = (*queue);
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = item;
+}
 
-    return item;
+priority_queue *extract_minimum(priority_queue **queue)
+{
+    int minimum_weight = (*queue)->key;
+    priority_queue *minimum_item = (*queue), *current = (*queue), *before = NULL;
+
+    while (current != NULL)
+    {
+        if (current->key < minimum_weight)
+        {
+            minimum_weight = current->key;
+            minimum_item = current;
+        }
+        current = current->next;
+    }
+    current = (*queue);
+
+    while (current != NULL)
+    {
+        if (minimum_item == current)
+        {
+            if (before == NULL)
+            {
+                (*queue) = minimum_item->next;
+            }
+            else
+            {
+                before->next = minimum_item->next;
+            }
+            minimum_item->next = NULL;
+            break;
+        }
+        before = current;
+        current = current->next;
+    }
+
+    return minimum_item;
 }
 
 priority_queue *get_priority_queue(graph *list)
@@ -47,37 +95,76 @@ priority_queue *get_priority_queue(graph *list)
 
     for (int i = 0; i < list->vertex_list_size; i++)
     {
+        if (list->vertex_list[i]->adj_list == NULL)
+        {
+            continue;
+        }
+
         int minimum_weight_value = list->vertex_list[i]->adj_list->weight;
-        node *current = list->vertex_list[i]->adj_list, *edge_to_save;
-        edge_to_save = current;
+        node *current = list->vertex_list[i]->adj_list;
 
         while (current != NULL)
         {
             if (current->weight < minimum_weight_value)
             {
                 minimum_weight_value = current->weight;
-                edge_to_save = current;
             }
             current = current->next;
         }
 
-        enqueue(&queue, (*edge_to_save));
+        enqueue(&queue, ((*list).vertex_list[i]));
     }
 
     return queue;
 }
 
-void print_queue(priority_queue *queue)
+priority_queue *get_vertex_by_edge(priority_queue *queue, node *edge)
 {
     priority_queue *current = queue;
-    int index = 0;
+
     while (current != NULL)
     {
-        printf("%d - Value:%d|Weight:%d\n", index, current->edge.value, current->edge.weight);
-
+        if (current->current_vertex->id == edge->value)
+        {
+            return current;
+        }
         current = current->next;
-        index++;
     }
+    return NULL;
+}
+
+void prim(graph **list)
+{
+    priority_queue *queue = get_priority_queue((*list)), *used_queue = NULL;
+
+    (*queue).key = 0;
+
+    while (queue != NULL)
+    {
+        priority_queue *item = extract_minimum(&queue);
+        enqueue_2(&used_queue, item);
+        vertex *current_vertex = item->current_vertex;
+        node *current_edge = current_vertex->adj_list;
+
+        while (current_edge != NULL)
+        {
+            priority_queue *adj_vertex = get_vertex_by_edge(queue, current_edge);
+
+            if (adj_vertex == NULL)
+            {
+                current_edge = current_edge->next;
+                continue;
+            }
+
+            if (current_edge->weight < adj_vertex->key)
+            {
+                adj_vertex->key = current_edge->weight;
+                adj_vertex->parent = current_vertex;
+            }
+            current_edge = current_edge->next;
+        }
+    }
+    printf("AAAAAAAAAAAAAAAAAA");
 }
 
 int main()
@@ -107,7 +194,5 @@ int main()
     show_graph(list1);
     printf("=================\n");
 
-    priority_queue *queue = get_priority_queue(list1);
-    print_queue(queue);
-    printf("=================\n");
+    prim(&list1);
 }
